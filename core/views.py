88a3e52e8955b2fe7805a894dc47014a9b69c20c
Fabio Oliveira <oliveirafabiopereirade@gmail.com>
2020-07-22 15:11:26 -0300

    Tenta criar uma rota que retorne o local do evento através do nome do evento

--- core/views.py
+++ core/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
+from core.models import Evento
 
 # Create your views here.
+
+
+
+def eventos(request, titulo_evento):
+    # consulta = Evento.objects.get(titulo = titulo_evento)
+    # local = objeto.consulta
+    # return HttpResponse(local)
+    return HttpResponse('Hello World!')
