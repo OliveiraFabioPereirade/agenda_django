4e2e85811cf9578e65d96622548701c407de07fb
Fabio Oliveira <oliveirafabiopereirade@gmail.com>
2020-07-22 14:17:16 -0300

    Cria filtros de eventos por título, usuário e data

--- core/admin.py
+++ core/admin.py
@@ -5,6 +5,7 @@ from core.models import Evento # importa classe Evento
 
 class EventoAdmin(admin.ModelAdmin):
     list_display = ('titulo', 'data_evento', 'data_criacao') # cria lista que exibe elementos dos eventos
+    list_filter = ('titulo', 'usuario', 'data_evento',) # cria lista de filtro por titulo, usuário e data
 
 admin.site.register(Evento, EventoAdmin) # registra tabela Evento e lista EventoAdmin
 
